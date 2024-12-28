611b5d41db02ff1889aaced17c42f6443efe6104
Neha-9824 <neha.nimje.5057@gmail.com>
2024-12-28 22:38:15 +0530

    add index.py

---
+++ index.py
@@ -0,0 +1,25 @@
+'''Python is a computer programming language often used to build websites and software, 
+automate tasks, and conduct data analysis. Python is a general-purpose language, meaning it can 
+be used to create a variety of different programs and isn't specialized for any specific problems.3 Apr 2024
+'''
+
+'''Python is an interpreted, object-oriented, high-level programming language with dynamic semantics developed by Guido van Rossum. 
+It was originally released in 1991. 
+Designed to be easy as well as fun, the name "Python" is a nod to the British comedy group Monty Python.
+
+'''
+
+'''Python is a programming language that is widely used in web applications, software development, data science, and machine learning (ML). 
+Developers use Python because it is efficient and easy to learn and can run on many different platforms.
+
+'''
+
+'''Python is a programming language that is interpreted, object-oriented, and considered to be high-level too. 
+What is Python? Python is one of the easiest yet most useful programming languages which is widely used in the software industry. 
+People use Python for Competitive Programming, Web Development, and creating software. Due to its easiest syntax, 
+it is recommended for beginners who are new to the software engineering field. 
+Its demand is growing at a very rapid pace due to its vast use cases in Modern Technological fields like Data Science, Machine learning, 
+and Automation Tasks. For many years now, it has been ranked among the top Programming languages.
+
+
+'''
